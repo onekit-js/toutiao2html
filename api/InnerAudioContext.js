ff30e7657e51f6e7896504358f3cbdd0809f232f
WangYeWei <826036140@qq.com>
2021-01-18 16:33:22 +0800

    Update InnerAudioContext.js

--- api/InnerAudioContext.js
+++ api/InnerAudioContext.js
@@ -1,10 +1,30 @@
 export default class InnerAudioContext{
   constructor(innerAudioContext) {
     this.innerAudioContext = innerAudioContext
+    this.innerAudioContext.currentTime = 0
+    this.innerAudioContext.autoplay = true
+    this.innerAudioContext.loop = true
+    this.innerAudioContext.muted = true
   }
 
   set src(src) {
     this.innerAudioContext.src = src
+  }
+
+  set startTime(position) {
+    this.innerAudioContext.currentTime = position
+  }
+
+  set autoplay(boolean) {
+    this.innerAudioContext.autoplay = boolean
+  }
+
+  set loop(boolean) {
+    this.innerAudioContext.loop = boolean
+  }
+
+  set obeyMuteSwitch(boolean) {
+    this.innerAudioContext.muted =  boolean
   }
 
   play() {
