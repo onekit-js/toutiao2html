cad26270897898b360d7f1c2f060bd8b8d7684ff
WangYeWei <826036140@qq.com>
2021-01-20 15:32:01 +0800

    Update FileSystemManager.js

--- api/FileSystemManager.js
+++ api/FileSystemManager.js
@@ -6,11 +6,11 @@ export default class FileSystemManager {
   }
 
   accessSync(path) {
-    if(!path) throw new Error('path is invalid')
-    if(path.substr(0, 6) !== 'ttfile') throw new Error('Browser is not support read the user disk')
-    if(Object.keys(this.fso.TEMP).indexOf(path) !== -1) {
+    if (!path) throw new Error('path is invalid')
+    if (path.substr(0, 6) !== 'ttfile') throw new Error('Browser is not support read the user disk')
+    if (Object.keys(this.fso.TEMP).indexOf(path) !== -1) {
       return true
-    }else {
+    } else {
       throw new Error(`accessSync:fail no such file or directory, accessSync ${path}`)
     }
   }
@@ -23,19 +23,30 @@ export default class FileSystemManager {
     options = null
 
     PROMISE(SUCCESS => {
-      if(!path) throw new Error('path is invalid')
-      if(path.substr(0, 6) !== 'ttfile') throw new Error('Browser is not support read the user disk')
-      if(Object.keys(this.fso.TEMP).indexOf(path) !== -1) {
+      if (!path) throw new Error('path is invalid')
+      if (path.substr(0, 6) !== 'ttfile') throw new Error('Browser is not support read the user disk')
+      if (Object.keys(this.fso.TEMP).indexOf(path) !== -1) {
         const res = {
           errMsg: 'access: ok'
         }
-       SUCCESS(res)
+        SUCCESS(res)
       } else {
         const res = {
           errMsg: `access:fail no such file or directory, access ${path}`
         }
-       throw Error(res)
+        throw Error(res)
       }
     }, success, complete, fail)
+  }
+
+  saveFileSync(tempFilePath, filePath) {
+    try {
+      const blob = this.fn_global().TEMP[tempFilePath]
+      const filename = blob.type
+      const savedFilePath = filePath || TheKit.createTempPath(filename)
+      this.fn_global().FSO[savedFilePath] = blob
+    } catch (e) {
+      throw new Error(e)
+    }
   }
 }
