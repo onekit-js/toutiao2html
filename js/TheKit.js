616dd259b3657935fcd275a8cd7388c79bbaf264
WangYeWei <826036140@qq.com>
2021-01-22 14:30:58 +0800

    .

--- js/TheKit.js
+++ js/TheKit.js
@@ -159,7 +159,6 @@ export default class TheKit {
     }
   }
 
-
   static fixurl(wx_rel_url) {
     const wx_abs_url = PATH.res2abs(currentUrl(), wx_rel_url)
     if (Vue.prototype.APP_JSON.pages.indexOf(wx_abs_url) < 0) {
@@ -170,7 +169,6 @@ export default class TheKit {
     const vue_path = wx_abs_url // ...
     return vue_path
   }
-
 
   header2json(str) {
     var strArray = str.split('\n');
@@ -374,9 +372,46 @@ export default class TheKit {
     reader.readAsBinaryString(blob)
   }
 
+  static base64decode(e) {
+    const base64DecodeChars = new Array((-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), 62, (-1), (-1), (-1), 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, (-1), (-1), (-1), (-1), (-1), (-1), (-1), 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, (-1), (-1), (-1), (-1), (-1), (-1), 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, (-1), (-1), (-1), (-1), (-1));
+    let r, a, c, h, o, t, d;
+    for (t = e.length, o = 0, d = ''; o < t;) {
+      do r = base64DecodeChars[255 & e.charCodeAt(o++)]
+      while (o < t && r == -1);
+      if (r == -1) break;
+      do a = base64DecodeChars[255 & e.charCodeAt(o++)]
+      while (o < t && a == -1);
+      if (a == -1) break;
+      d += String.fromCharCode(r << 2 | (48 & a) >> 4)
+      do {
+        if (c = 255 & e.charCodeAt(o++), 61 == c) return d
+        c = base64DecodeChars[c]
+      } while (o < t && c == -1)
+      if (c == -1) break;
+      d += String.fromCharCode((15 & a) << 4 | (60 & c) >> 2)
+      do {
+        if (h = 255 & e.charCodeAt(o++), 61 == h) return d;
+        h = base64DecodeChars[h]
+      } while (o < t && h == -1)
+      if (h == -1) break;
+      d += String.fromCharCode((3 & c) << 6 | h)
+    }
+    return d
+  }
+
   static blob2hex(blob, callback) {
-   this.blobToBase64(blob, res => {
-    console.log(res.toString(16))
-   })
+    this.blobToBase64(blob, res => {
+      console.log(res.toString(16))
+    })
+    this.blobToBase64(blob, res => {
+      let bin = this.base64decode(res.replace(/[ \r\n]+$/, ""))
+      let hex = []
+      for (let i = 0; i < bin.length; ++i) {
+        let tmp = bin.charCodeAt(i).toString(16);
+        if (tmp.length === 1) tmp = "0" + tmp;
+        hex[hex.length] = tmp;
+      }
+      callback(hex.join(''))
+    })
   }
 }
